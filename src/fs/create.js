3d27a83d47814b163535c855d3ccc337736c5fe4
Sergey Tsepodoy <sp1tfire_90@mail.ru>
2024-10-04 21:21:30 +0500

    feat: add solution for create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,14 @@
+import { writeFile } from "node:fs/promises";
+
+const path = "./src/fs/files/fresh.txt";
+const content = "I am fresh and young";
+
 const create = async () => {
-    // Write your code here 
+  try {
+    await writeFile(path, content, { flag: "wx+" });
+  } catch (err) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await create();

d2b8413395a0074801ac765e8a2e816eeab53d3d
Sergey Tsepodoy <sp1tfire_90@mail.ru>
2024-10-07 10:20:02 +0500

    feat: add solution for env function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
+import { env } from "node:process";
+
 const parseEnv = () => {
-    // Write your code here 
+  const result = [];
+  for (const [key, value] of Object.entries(env)) {
+    if (key.startsWith("RSS_")) {
+      result.push(`${key}=${value}`);
+    }
+  }
+  console.log(result.join("; "));
 };
 
 parseEnv();
